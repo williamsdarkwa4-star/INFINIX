efa2f029d169030fbe1483ab258ed5e91a1dfdca
williamsdarkwa4-star <williamsdarkwa4@gmail.com>
2026-08-09 05:21:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1175,6 +1175,9 @@ with app.app_context():
         print("Database initialization skipped:", exc)
 
 
-if __name__ == "__main__":
+
+if__name__ == "__main__":
     port = int(os.environ.get("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False
+           init_db() 
+    
