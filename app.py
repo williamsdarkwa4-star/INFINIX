8e7606879ac64eabbfeb5ae6187da32da1aa0641
williamsdarkwa4-star <williamsdarkwa4@gmail.com>
2026-08-09 05:31:01 +0000

    Remove init_db call from main execution block

--- app.py
+++ app.py
@@ -1179,5 +1179,5 @@ with app.app_context():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "5000"))
     app.run(host="0.0.0.0", port=port, debug=False)
-           init_db() 
+           
     
