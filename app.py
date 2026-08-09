09b5b5605564c6f1921093b3b299089b40a29744
williamsdarkwa4-star <williamsdarkwa4@gmail.com>
2026-08-09 05:24:47 +0000

    Fix formatting of main entry point condition

--- app.py
+++ app.py
@@ -1176,7 +1176,7 @@ with app.app_context():
 
 
 
-if__name__ == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", "5000"))
     app.run(host="0.0.0.0", port=port, debug=False
            init_db() 
