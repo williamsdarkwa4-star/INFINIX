154c70af2d7357bb53db738059691ef692f6c5fc
williamsdarkwa4-star <williamsdarkwa4@gmail.com>
2026-08-12 03:54:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1639,7 +1639,7 @@ def my_plan():
     return render_template(
         "my_plan.html",
         user_plans=plans,
-        plans=plans
+        plans=plans,
         active_plans=all_plans,  # Keep old template variable but include every historical purchase.
         all_plans=all_plans,
         plans=available_plans,
